91455dc7baa738a98d989f2ddd55bd5094845471
johnspeny <johnspeny@gmail.com>
2021-12-25 21:27:14 +0300

    Add box2d userdata on sprite

--- Classes/collision_system.cpp
+++ Classes/collision_system.cpp
@@ -7,7 +7,7 @@ void CollisionSystem::configure(EventManager& events)
 	GameVars::initVars();
 	// create a b2d world
 	b2Vec2 gravity;
-	gravity.Set(0.0f, -9.8f);
+	gravity.Set(0.0f, 0.0f);
 
 	// create the world
 	_world = b2WorldNode::create(gravity.x, gravity.y, GameVars::metersHeight);
@@ -19,21 +19,67 @@ void CollisionSystem::receive(const ComponentAddedEvent<CollisionComponent>& col
 {
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 
-	// bridge
-	b2BodyDef bridgeDef;
-	bridgeDef.type = b2_staticBody;
-	bridgeDef.position.Set(visibleSize.width * 0.5f / GameVars::PTM_Ratio, visibleSize.height * 0.1f / GameVars::PTM_Ratio);
-	b2Body* bridgeBody = _world->getb2World()->CreateBody(&bridgeDef);
+	auto spriteEntity = collisionComponentAddedEvent.entity;
+	
+	if (!spriteEntity.has_component<SpriteComponent>())
+	{
+		// error
+		log("No sprite component");
+	}
+	else
+	{
+		auto sprite = spriteEntity.component<SpriteComponent>()->sprite;
 
-	// bridge shape
-	b2PolygonShape bridgeShape;
-	bridgeShape.SetAsBox(0.5f, 0.5f);
 
-	// connect body to shape
-	bridgeBody->CreateFixture(&bridgeShape, 0.0f);
+		//// bridge
+		//b2BodyDef bridgeDef;
+		//bridgeDef.type = b2_dynamicBody;
+		//bridgeDef.position.Set(sprite->getPositionX() / GameVars::PTM_Ratio, sprite->getPositionY() / GameVars::PTM_Ratio);
+		//
+		//CollidibleSprite* actor = sprite;
+		//bridgeDef.userData.pointer = reinterpret_cast<uintptr_t>(actor);
+		//b2Body* bridgeBody = _world->getb2World()->CreateBody(&bridgeDef);
+
+		//// bridge shape
+		//b2PolygonShape bridgeShape;
+		//bridgeShape.SetAsBox(0.5f, 0.5f);
+
+		//// connect body to shape
+		//bridgeBody->CreateFixture(&bridgeShape, 0.0f);
+
+
+
+
+		b2BodyDef def;
+		def.type = b2BodyType::b2_dynamicBody;
+		def.position.Set(sprite->getPositionX() / GameVars::PTM_Ratio, sprite->getPositionY() / GameVars::PTM_Ratio);
+		CollidibleSprite* actor = sprite;
+		def.userData.pointer = reinterpret_cast<uintptr_t>(actor);
+
+		auto body = _world->getb2World()->CreateBody(&def);
+		b2PolygonShape shape;
+		shape.SetAsBox(0.5, 0.5);
+		body->CreateFixture(&shape, 1);
+	}
+	
 }
 
 void CollisionSystem::update(EntityManager& es, EventManager& events, double dt)
 {
 	_world->getb2World()->Step(dt, 6, 2);
+
+	ComponentHandle<SpriteComponent> spriteComponent;
+	for (auto entity : es.entities_with_components(spriteComponent))
+	{
+		auto sprite = spriteComponent->sprite;
+		for (b2Body* body = _world->getb2World()->GetBodyList(); body != nullptr; body = body->GetNext()) {
+			if (body->GetUserData().pointer == reinterpret_cast<uintptr_t>(sprite)) {
+				CollidibleSprite* spr = (CollidibleSprite*)body->GetUserData().pointer;
+				spr->setPosition(Vec2(body->GetPosition().x * GameVars::PTM_Ratio, body->GetPosition().y * GameVars::PTM_Ratio));
+				spr->setRotation(-1 * CC_RADIANS_TO_DEGREES(body->GetAngle()));
+			}
+		}
+	}
+
+
 }
